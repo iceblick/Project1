c9e37deaf904d389ebcb794308304ed0de6e6108
Iceblick <ruben.vanegas@cwc.com>
2021-08-23 16:52:51 -0500

    Colocando las troncales en la pagina inicial

--- Trunks/views.py
+++ Trunks/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Trunk_OTN
+from .models import Trunk_DWDM
+from .models import Trunk_SDH
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'Trunks/post_list.html', {})
+	posts1 = Trunk_OTN.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+	posts2 = Trunk_DWDM.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+	posts3 = Trunk_SDH.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+	return render(request, 'Trunks/post_list.html', {'posts1': posts1, 'posts2': posts2, 'posts3': posts3})
